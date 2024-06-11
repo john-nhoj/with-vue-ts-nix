7f8d1744de94daea22b609c8501821358b9787e3
John Cheng (john-nhoj) <cheng.john@pm.me>
2024-06-11 22:17:01 +0100

    feat: setup tests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 data-testid="hello-world">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
