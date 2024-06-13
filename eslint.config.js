38186a90619aafb771fdd72f9376094929a7814c
John Cheng (john-nhoj) <cheng.john@pm.me>
2024-06-13 21:45:37 +0100

    chore: use only eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,26 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      "object-curly-spacing": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }],
+      "no-multi-spaces": "error",
+      "no-mixed-spaces-and-tabs": "error",
+      "no-irregular-whitespace": "error",
+      "no-console": "error",
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.strict,
   ...pluginVue.configs["flat/recommended"],
-  eslintConfigPrettier,
 ];
--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -1,17 +1,17 @@
-import HelloWorld from './HelloWorld.vue'
-import { mount } from '@vue/test-utils'
+import HelloWorld from './HelloWorld.vue';
+import { mount } from '@vue/test-utils';
 
 describe('HelloWorld.vue', () => {
   const setup = () => {
     const wrapper = mount(HelloWorld, {
       props: {
-        msg: 'Hello, world!'
-      }
-    })
-    return { wrapper }
-  }
+        msg: 'Hello, world!',
+      },
+    });
+    return { wrapper };
+  };
   it('renders correctly', () => {
-    const { wrapper } = setup()
-    expect(wrapper.find('[data-testid="hello-world"]').text()).toBe('Hello, world!')
-  })
-})
+    const { wrapper } = setup();
+    expect(wrapper.find('[data-testid="hello-world"]').text()).toBe('Hello, world!');
+  });
+});
